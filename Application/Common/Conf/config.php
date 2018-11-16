9ba511397dbaebeae10fa6f8b94cf3353a2bda5c
swyuliang <36142847+swyuliang@users.noreply.github.com>
2018-11-16 23:28:04 +0800

    swyuliang

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'DB_TYPE'=>'mysql',
+	'DB_HOST'=>'127.0.0.1',
+	'DB_NAME'=>'php34',
+	'DB_USER'=>'root',
+	'DB_PWD'=>'',
+	'DB_PREFIX'=>'php34_',
+	'DB_CHARSET'=>'utf8',
+	'DB_PORT'=>'3306',
+);
